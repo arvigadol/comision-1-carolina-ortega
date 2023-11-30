62f750221210002b8674391aeedb906602f0b227
Carolina <arvigadol@gmail.com>
2023-11-30 18:46:36 -0300

    [FEAT] Se creó la carpeta settings y se crearon los archivos config.js y database.js. Se configuraron además las variables de entorno. En este punto el servidor ya funciona.

---
+++ BACKEND/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import 'dotenv/config';
+
+const app = express();
+
+import { env } from "./src/settings/config.js";
+import { startConnection } from "./src/settings/database.js";
+
+//Middlewares
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  }));
+app.use(morgan('dev'));
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+
+app.listen(env.port, async () => {
+    await startConnection({ uri: env.mongo, database: env.database });
+    console.log(`Servidor corriendo en http://localhost:${env.port}`)
+});
